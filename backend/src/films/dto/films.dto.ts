0e8e892585410738ad5939ac650f7abc5b51211e
ismaxse <ismaxse@gmail.com>
2026-08-14 11:54:28 +0300

    добавлена валидация DTO

--- backend/src/films/dto/films.dto.ts
+++ backend/src/films/dto/films.dto.ts
@@ -1,32 +1,88 @@
+import {
+  IsArray,
+  IsInt,
+  IsNumber,
+  IsString,
+  IsUUID,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class ScheduleDto {
+  @IsUUID()
   id!: string;
+
+  @IsString()
   daytime!: string;
+
+  @IsInt()
   hall!: number;
+
+  @IsInt()
   rows!: number;
+
+  @IsInt()
   seats!: number;
+
+  @IsNumber()
   price!: number;
+
+  @IsArray()
+  @IsString({ each: true })
   taken!: string[];
 }
 
 export class FilmDto {
+  @IsUUID()
   id!: string;
+
+  @IsNumber()
   rating!: number;
+
+  @IsString()
   director!: string;
+
+  @IsArray()
+  @IsString({ each: true })
   tags!: string[];
+
+  @IsString()
   title!: string;
+
+  @IsString()
   about!: string;
+
+  @IsString()
   description!: string;
+
+  @IsString()
   image!: string;
+
+  @IsString()
   cover!: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ScheduleDto)
   schedule!: ScheduleDto[];
 }
 
 export class FilmsResponseDto {
+  @IsInt()
   total!: number;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => FilmDto)
   items!: FilmDto[];
 }
 
 export class FilmScheduleResponseDto {
+  @IsInt()
   total!: number;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ScheduleDto)
   items!: ScheduleDto[];
 }
